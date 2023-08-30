c26cf4f60ea4874f39ca70bf5464b8c722a1e28e
isurojit <iamsurojit@hotmail.com>
2023-08-30 07:33:07 +0530

    Pagination completed

--- js/script.js
+++ js/script.js
@@ -279,6 +279,10 @@ const search = async () => {
 
 //Display search results
 const displaySearchResults = (results) => {
+  //Clear Previous results
+  document.querySelector("#search-results").innerHTML = "";
+  document.querySelector("#search-results-heading").innerHTML = "";
+  document.querySelector("#pagination").innerHTML = "";
   results.forEach((result) => {
     const div = document.createElement("div");
     div.classList.add("card");
@@ -314,6 +318,42 @@ const displaySearchResults = (results) => {
       <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
     `;
     document.querySelector("#search-results").appendChild(div);
+  });
+  displayPagination();
+};
+
+//create display pagination
+const displayPagination = () => {
+  const div = document.createElement("div");
+  div.classList.add("pagination");
+  div.innerHTML = `
+  <button class="btn btn-primary" id="prev">Prev</button>
+          <button class="btn btn-primary" id="next">Next</button>
+          <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>`;
+  document.querySelector("#pagination").appendChild(div);
+
+  //Disable previe btn if on first page
+  if (global.search.page === 1) {
+    document.querySelector("#prev").disabled = true;
+  }
+
+  //Disable next btn if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector("#next").disabled = true;
+  }
+
+  //Next Page
+  document.querySelector("#next").addEventListener("click", async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  //Next Page
+  document.querySelector("#prev").addEventListener("click", async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
   });
 };
 
@@ -386,7 +426,7 @@ const searchAPIData = async () => {
   const API_URL = "https://api.themoviedb.org/3/";
   showSpinner();
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
   const data = await response.json();
   setTimeout(function () {
